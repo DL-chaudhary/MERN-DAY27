74f98b0cc67af60b3624595f2eb3cabe875db141
DL-chaudhary <kamalnarayan981475@gmail.com>
2025-09-28 11:12:30 +0545

    Create blog

--- src/Componenr/Navbar.jsx
+++ src/Componenr/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <>
       <div className="navbar">
     <a href="#">Home</a>
-    <button className="add-btn">Add</button>
+   <Link to="/login"> <button className="add-btn">Add</button></Link>
   </div>
     </>
   )
